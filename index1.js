b8be635cf43d2c0e34a6bdbf9c7ff222c79aa3ea
Sahil Vasuja <sahilvasuja@Sahil-Vasujas-MacBook-Pro.local>
2023-01-04 18:24:49 +0530

    crud

--- index1.js
+++ index1.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-const data = { x: 5 };
-// create
+const data = { x: 5, y:7 };
+// // create
 fs.writeFile('store.json', JSON.stringify(data), (err) => {
   if (err) throw err;
   console.log('Data written to file');
+  
 });
-// read
+
+// //read
 fs.readFile('store.json', (err, data) => {
     if (err) throw err;
   
@@ -19,16 +21,22 @@ fs.readFile('store.json', (err, data) => {
   
     const jsonData = JSON.parse(data);
     jsonData.x = 10;
-  
+    
     fs.writeFile('store.json', JSON.stringify(jsonData), (err) => {
-      if (err) throw err;
+      if (err) throw err; 
       console.log('Data updated');
     });
   });
 
 
+  // fs.writeFile('store.json', '{"y":7}', (err) => {
+  //   if (err) throw err;
+    
+  //   console.log('Data deleted');
+  // });
+
 //   delete
-fs.unlink('store.json', (err) => {
-  if (err) throw err;
-  console.log('File deleted');
-});
+// fs.unlink('store.json', (err) => {
+//   if (err) throw err;
+//   console.log('File deleted');
+// });
