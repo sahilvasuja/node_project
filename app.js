5a3e99a332c45dcc0b7f0d6e8534653af836bae2
ruchik02 <ruchikasharma.cse19@chitkarauniversity.edu.in>
2023-01-05 13:42:58 +0530

    todo style

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const fs=require('fs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/add-todo', (req, res) => {
+    const todo = req.body.todo;
+    console.log("todo",todo);
+    fs.readFile('database.json', (err, data) => {
+      if (err) throw err;
+  
+      let jsonData = JSON.parse(data);
+      jsonData.push(todo);
+  
+      fs.writeFile('database.json', JSON.stringify(jsonData), (err) => {
+        if (err) throw err;
+        console.log('Todo added');
+        res.redirect('/');
+      });
+    });
+  });
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
