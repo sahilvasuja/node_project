b9aea8bcc771f890008b2f0781f76ed8c5b6657c
ruchik02 <ruchikasharma.cse19@chitkarauniversity.edu.in>
2023-01-09 18:48:21 +0530

    checkbox functionality

--- app.js
+++ app.js
@@ -4,37 +4,78 @@ const fs=require('fs');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
+// home
 app.get('/', (req, res) => {
   fs.readFile('database.json',(err,data)=>{
     if(err) throw err;
     let jsonData=JSON.parse(data);
-    res.render('index',{todo:jsonData});
+    res.render('index',{todoValue:jsonData});
   })
   // res.render('index');
 });
-
+// add-todo
 app.post('/add-todo', (req, res) => {
-    const todo = req.body.todo;
-    console.log("todo 13",todo);
+    const result = req.body.todo;
+    console.log("19",result);
     fs.readFile('database.json', (err, data) => {
       if (err) throw err;
-      console.log("todo 16",todo);
+      console.log("22",result);
       let jsonData = JSON.parse(data);
+      const todo = {
+        task: result,
+        isCompleted: false,
+        isUpdated:false,
+        id:Date.now()
+      };
       jsonData.push(todo);
       fs.writeFile('database.json', JSON.stringify(jsonData), (err) => {
         if (err) throw err;
         console.log('Todo added');
-        // res.redirect('/');
-        res.render('index',{todo:jsonData});
-        console.log(jsonData);
+        res.render('index',{todoValue:jsonData});
+        console.log("34",jsonData);
       });
     });
   });
-
-  app.get('/delete-todo/:id',(req,res)=>{
+  // check
+  app.get("/check/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id,"41")
+    fs.readFile("database.json", (err, data) => {
+      if (err) throw err;
+      const jsonData = JSON.parse(data);
+      jsonData[id].isCompleted=!jsonData[id].isCompleted
+      console.log("47",jsonData);
+      fs.writeFile("database.json", JSON.stringify(jsonData), (err) => {
+        if (err) throw err;
+        console.log("checked");
+      });
+    });
+    res.redirect("/");
+  });
+  // update
+  // app.get("/update-todo/:id", (req, res) => {
+  //   const id = req.params.id;
+  //   console.log(id, "58");
+  //   fs.readFile("database.json", (err, data) => {
+  //     if (err) throw err;
+  //     // const result = req.body.todo;
+  //      const jsonData = JSON.parse(data);
+  //     const todo = jsonData.find(todo => todo.id === id);
+  //     //  const todoId = todo.id;
+  //      console.log(`The ID of the todo is ${todoId}`);
+  //     fs.writeFile("database.json", JSON.stringify(jsonData), (err) => {
+  //       if (err) throw err;
+  //       const todoId=req.params.id;
+  //       console.log("76",todoId);
+  //       console.log("Data updated");
+  //     });
+  //   });
+  //   res.redirect("/");
+  // });
+// delete
+  app.get('/delete-todo/:id',async(req,res)=>{
     const id=req.params.id;
-    // console.log(id);
+    console.log("59",id);
     fs.readFile('database.json',(err,data)=>{
       if(err)
       throw err;
@@ -43,13 +84,12 @@ app.post('/add-todo', (req, res) => {
       fs.writeFile('database.json', JSON.stringify(jsonData), (err) => {
         if (err) throw err;
         console.log('Data written to file');
+        console.log("68",jsonData)
       });
     });
-    console.log("Deleted Todo Successfully!");
+    console.log("Todo is deleted Successfully!");
           res.redirect("/");
   });
-
-  
 
 app.listen(3000, () => {
   console.log('App listening on port 3000');
